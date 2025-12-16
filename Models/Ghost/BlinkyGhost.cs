907f44c3613e4626bec77232e541ab80bccfa57a
Юлия Сазонова <pesnaychek@gmail.com>
2025-12-17 02:07:26 +0700

    Reapply " Добавлен GameManager, класс Ghost, созданы 4 конкретных класса привидений."
    This reverts commit c747efc8d13d77c9e24662ea353a2cafb1fc9799.

---
+++ Models/Ghost/BlinkyGhost.cs
@@ -0,0 +1,16 @@
+﻿using pacman3.Utils;
+using System.Windows.Media;
+
+namespace pacman3.Models.Ghosts
+{
+    public class BlinkyGhost : Ghost // преследует напрямую
+    {
+        protected override Color OriginalColor => Colors.Red;
+
+        public override Vector2 CalculateTargetPosition(Vector2 playerPosition, Direction playerDirection)
+        {
+            // Прямое преследование игрока
+            return playerPosition;
+        }
+    }
+}
---
+++ Models/Ghost/PinkyGhost.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media;
+using pacman3.Utils;
+
+namespace pacman3.Models.Ghosts
+{
+    public class PinkyGhost : Ghost // пытается зайти спереди
+    {
+        protected override Color OriginalColor => Colors.Pink;
+
+        public override Vector2 CalculateTargetPosition(Vector2 playerPosition, Direction playerDirection)
+        {
+            // Заход на 4 тайла вперед от игрока
+            Vector2 target = playerPosition;
+
+            switch (playerDirection)
+            {
+                case Direction.Up:
+                    target.Y -= 4 * 32;
+                    break;
+                case Direction.Down:
+                    target.Y += 4 * 32;
+                    break;
+                case Direction.Left:
+                    target.X -= 4 * 32;
+                    break;
+                case Direction.Right:
+                    target.X += 4 * 32;
+                    break;
+            }
+
+            return target;
+        }
+    }
+}
