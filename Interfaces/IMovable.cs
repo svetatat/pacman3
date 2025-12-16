ea89b87541f01b8fdae0a0c9fe8071721d95e535
Юлия Сазонова <pesnaychek@gmail.com>
2025-12-16 22:43:07 +0700

    создание класса игрок

--- Interfaces/IMovable.cs
+++ Interfaces/IMovable.cs
@@ -1,12 +1,11 @@
 ﻿using pacman3.Utils;
-using System.Windows;
 
 namespace pacman3.Interfaces
 {
-    public interface IMovable // Интерфейс для перемещаемых объектов
+    public interface IMovable
     {
-        System.Numerics.Vector Position { get; set; }
-        System.Numerics.Vector Velocity { get; set; }
-        void Move(System.Numerics.Vector direction);
+        Vector2 Position { get; set; }
+        Vector2 Velocity { get; set; }
+        void Move(Vector2 direction);
     }
 }
