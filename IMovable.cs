924f4133dfc28d61d8ba57b076512eb5bf8cd6ee
Юлия Сазонова <pesnaychek@gmail.com>
2025-12-16 19:54:13 +0700

    интерфейс IMovable

---
+++ IMovable.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace PacManGame.Models.Interfaces
+{
+    /// <summary>
+    /// Интерфейс для перемещаемых объектов
+    /// </summary>
+    public interface IMovable
+    {
+        /// <summary>
+        /// Позиция объекта
+        /// </summary>
+        Vector Position { get; set; }
+
+        /// <summary>
+        /// Скорость объекта
+        /// </summary>
+        Vector Velocity { get; set; }
+
+        /// <summary>
+        /// Перемещение объекта
+        /// </summary>
+        /// <param name="direction">Направление движения</param>
+        void Move(Vector direction);
+    }
+}
