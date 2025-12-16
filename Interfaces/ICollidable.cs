a5d421dfc9492f4bfd03b8b27b2f05822ad81939
svetatat <titevalova.val@gmail.com>
2025-12-16 19:33:29 +0700

    добавлено три интерфейса

---
+++ Interfaces/ICollidable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace pacman3.Interfaces
+{
+    public interface ICollidable
+    {
+        Rect Bounds { get; } // Границы объекта для проверки столкновений
+        void OnCollision(ICollidable other); // Вызывается при столкновении с другим объектом
+    }
+}
