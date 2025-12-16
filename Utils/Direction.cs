6cdc2d481b38fc87751d1f1083cae719b4de633b
Юлия Сазонова <pesnaychek@gmail.com>
2025-12-16 19:59:46 +0700

    Создание классов: Utils, GameTime.

---
+++ Utils/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacManGame.Utils
+{
+    public enum Direction // Направления движения
+    {
+        None = 0,
+        Up = 1,
+        Down = 2,
+        Left = 3,
+        Right = 4
+    }
+}
